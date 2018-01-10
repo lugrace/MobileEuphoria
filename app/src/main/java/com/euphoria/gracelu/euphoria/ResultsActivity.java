8eef224c978fa30deba1f2ae85a0dedee57edf13
Grace Lu <2018glu@tjhsst.edu>
2018-01-10 14:09:24 -0500

    added happiness labels and sketched some functions

--- app/src/main/java/com/euphoria/gracelu/euphoria/ResultsActivity.java
+++ app/src/main/java/com/euphoria/gracelu/euphoria/ResultsActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 /**
  * Created by gracelu on 1/10/18.
  */
@@ -17,8 +19,18 @@ public class ResultsActivity extends AppCompatActivity{
         setContentView(R.layout.activity_results);
 
         TextView resultsName = (TextView) findViewById(R.id.resultsName);
+        TextView happinessIndex = (TextView) findViewById(R.id.happinessIndex);
         String collegeName = getIntent().getStringExtra("collegeName");
 
         resultsName.setText(collegeName);
     }
+
+    public String[] retrieveTweets(String collegeName){
+        return ["idk"];
+    }
+
+    public int findHappinessLevel(String[] tweets){
+        return 0;
+    }
+
 }
