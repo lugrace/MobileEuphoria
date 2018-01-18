010eaf72bcc221c1aa019ee707a5edf9f268ad11
Grace Lu <2018glu@tjhsst.edu>
2018-01-18 11:53:00 -0500

    set up things to pull in tweets w api

--- app/src/main/java/com/euphoria/gracelu/euphoria/ResultsActivity.java
+++ app/src/main/java/com/euphoria/gracelu/euphoria/ResultsActivity.java
@@ -4,9 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterApiClient;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Search;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.services.SearchService;
+import com.twitter.sdk.android.core.services.StatusesService;
 
 import org.w3c.dom.Text;
+
+import retrofit2.Call;
 
 /**
  * Created by gracelu on 1/10/18.
@@ -26,10 +37,37 @@ public class ResultsActivity extends AppCompatActivity{
         String collegeName = getIntent().getStringExtra("collegeName");
 
         resultsName.setText(collegeName);
+        String[] tweets = retrieveTweets(collegeName);
     }
 
     public String[] retrieveTweets(String collegeName){
         String[] tweets = new String[100];
+        TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
+        StatusesService statusesService = twitterApiClient.getStatusesService();
+        Call<Tweet> call = statusesService.show(524971209851543553L, null, null, null);
+        call.enqueue(new Callback<Tweet>() {
+            @Override
+            public void success(Result<Tweet> result) {
+                //Do something with result
+            }
+
+            public void failure(TwitterException exception) {
+                //Do something on failure
+            }
+        });
+
+        //testing
+        SearchService searchService = twitterApiClient.getSearchService();
+        Call<Search> call2 = searchService.tweets("Cornell", null, "en", null, null, 100, null, null, null, null);
+        call2.enqueue(new Callback<Search>() {
+            @Override
+            public void success(Result<Search> result) {
+                //do something with result
+            }
+            public void failure(TwitterException exception) {
+                //Do something on failure
+            }
+        });
 
         return tweets;
     }
